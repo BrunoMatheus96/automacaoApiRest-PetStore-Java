efa3a8f12370de537b015061f8d064d51f5fbdde
Bruno Matheus Stefano Leite <105085425+BrunoMatheus96@users.noreply.github.com>
2024-03-10 20:23:54 -0300

    Initial commit

---
+++ src/test/java/modulos/Produto/ProdutoTest.java
@@ -0,0 +1,14 @@
+package modulos.Produto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testes de API Rest do módulo de produto") //Título principal
+public class ProdutoTest {
+
+    @Test
+    @DisplayName("Validar os limites proibidos do valor do Produto") //Título do teste que vai aparecer nos relatórios e quando rodar
+    public void testValidarLimitesProibidosValorProduto(){
+    }
+}
